f39db249479ab0491f96e6a711b60e51dda21ab5
nhoEm <nguyenquoctruong110605@gmail.com>
2025-08-12 13:51:19 +0700

    Fouth commit: CRUD delete

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -30,12 +30,45 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('-------------------------------------------------------------------');
-    console.log(data);
-    console.log('-------------------------------------------------------------------');
+
     return res.render('displayCRUD.ejs', {
         dataTable: data
     });
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        return res.render('editCRUD.ejs', {
+            user: userData,
+        });
+    }
+    else {
+        return res.send('User not found');
+    }
+
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers,
+    });
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return displayGetCRUD(req, res);
+    }
+    else {
+        return res.send('Not found User');
+    }
+
 }
 
 module.exports = {
@@ -44,4 +77,7 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
