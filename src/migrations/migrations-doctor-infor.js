9df8bcb2de419aae7ff81099574e088f140c809f
nhoEm <nguyenquoctruong110605@gmail.com>
2026-01-03 16:07:24 +0700

    #78

---
+++ src/migrations/migrations-doctor-infor.js
@@ -0,0 +1,75 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        // : DataTypes.STRING,
+        // : DataTypes.INTEGER,
+        // : DataTypes.STRING,
+        // : DataTypes.STRING,
+        // : DataTypes.STRING,
+        // : DataTypes.STRING,
+        await queryInterface.createTable('doctor_Infor', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            doctorId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+
+            },
+            priceId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+
+            },
+
+            provinceId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+
+            },
+
+            paymentId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+
+            },
+
+            addressClinic: {
+                type: Sequelize.STRING,
+                allowNull: false,
+
+            },
+
+            nameClinic: {
+                type: Sequelize.STRING,
+                allowNull: false,
+
+            },
+
+            note: {
+                type: Sequelize.STRING
+            },
+
+            count: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0
+            },
+
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('doctor_Infor');
+    }
+};
